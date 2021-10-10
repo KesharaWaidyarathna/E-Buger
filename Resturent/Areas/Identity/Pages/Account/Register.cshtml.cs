870d946684528a12056e614c51c1922d202ead08
malithi1234 <malithialgewatta2000@gmail.com>
2021-10-10 20:31:45 +0530

    Add role changes

--- Resturent/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Resturent/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -84,25 +84,28 @@ namespace Resturent.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            string role = Request.Form["rdUserRole"].ToString();
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { 
-                    UserName = Input.Email, 
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Email,
                     Email = Input.Email,
-                    City= Input.City,
+                    City = Input.City,
                     StreetAddress = Input.StreetAddress,
                     State = Input.State,
                     PostalCode = Input.PostalCode,
-                    PhoneNumber = Input.PhoneNumber
+                    PhoneNumber = Input.PhoneNumber,
+                    Name = Input.Name
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    if(!await _roleManger.RoleExistsAsync(SD.ManagerUser))
+                    if (!await _roleManger.RoleExistsAsync(SD.ManagerUser))
                     {
                         await _roleManger.CreateAsync(new IdentityRole(SD.ManagerUser));
                     }
@@ -119,8 +122,24 @@ namespace Resturent.Areas.Identity.Pages.Account
                         await _roleManger.CreateAsync(new IdentityRole(SD.CustomerEndUser));
                     }
 
-                    await _userManager.AddToRoleAsync(user, SD.ManagerUser);
+                    switch (role)
+                    {
+                        case SD.KitchenUser:
+                            await _userManager.AddToRoleAsync(user, SD.KitchenUser);
+                            break;
+                        case SD.ManagerUser:
+                            await _userManager.AddToRoleAsync(user, SD.ManagerUser);
+                            break;
+                        case SD.FrontDeskUser:
+                            await _userManager.AddToRoleAsync(user, SD.FrontDeskUser);
+                            break;
+                        case SD.CustomerEndUser:
+                            await _userManager.AddToRoleAsync(user, SD.CustomerEndUser);
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+                    }
 
+                    return RedirectToAction("Index", "User", new { area = "Admin" });
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     //var callbackUrl = Url.Page(
@@ -138,8 +157,7 @@ namespace Resturent.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+
                     }
                 }
                 foreach (var error in result.Errors)
